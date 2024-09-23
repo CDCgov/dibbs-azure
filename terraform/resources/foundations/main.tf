6fcaec881187e417cd6a211b322d87b57cc60d42
Rin Concordia <rin@skylight.digital>
2024-09-23 13:36:28 -0500

    Remove duplicate key vault entry.

--- terraform/resources/foundations/main.tf
+++ terraform/resources/foundations/main.tf
@@ -9,23 +9,6 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-resource "azurerm_key_vault" "kv" {
-  name                        = "${var.team}${var.project}${var.env}kv"
-  location                    = var.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-
-  sku_name = "standard"
-
-  network_acls {
-    bypass         = "AzureServices"
-    default_action = "Deny"
-  }
-}
-
 resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.rg.location
   name                = "${var.team}${var.project}${var.env}acr"
