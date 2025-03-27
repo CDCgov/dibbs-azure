4e6e4e86432251c2570c2d4e8908eacc96da25b2
Rin Concordia <rin@skylight.digital>
2025-03-27 13:50:53 -0500

    Update DIBBs version to 3.0.0.

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -47,7 +47,7 @@ module "container_apps" {
   acr_username = module.foundations.acr_admin_username //TODO: Change to an ACA-specific password
   acr_password = module.foundations.acr_admin_password //TODO: Change to an ACA-specific password
 
-  dibbs_version = "v2.0.0-beta"
+  dibbs_version = "3.0.0"
 
   azure_storage_connection_string = module.foundations.azure_storage_connection_string
   azure_container_name            = module.foundations.azure_container_name
